746fb11a23a37fcb0bb341e231e34052fd316e0e
mohsin-8 <alikhanmohsin420@gmail.com>
2023-02-03 03:42:47 -0800

    update favicon

--- pages/_document.js
+++ pages/_document.js
@@ -8,6 +8,7 @@ export default function Document() {
           href="https://fonts.googleapis.com/css2?family=Caveat:wght@700&display=swap"
           rel="stylesheet"
         />
+        <link rel="shortcut icon" href="/logo.png" />
       </Head>
 
       <body>
